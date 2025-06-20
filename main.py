253cf71e6c6bd502214f2119f4d33b7e536e4033
WiseBoiii <aleksashin.1001@gmail.com>
2025-06-20 19:21:35 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import os
-
 import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+os.environ.setdefault(key='DJANGO_SETTINGS_MODULE', value='settings')
 django.setup()
 
 from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
-    # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    all_passcards = Passcard.objects.all()
+    active_passcards = Passcard.objects.filter(is_active=True)
+
+    print('Всего пропусков:', len(all_passcards))
+    print('Активных пропусков:', len(active_passcards))
+
--- settings.py
+++ settings.py
@@ -1,17 +1,29 @@
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+ENGINE = os.getenv('ENGINE')
+HOST = os.getenv('HOST')
+PORT = os.getenv('PORT')
+NAME = os.getenv('NAME')
+USER = os.getenv('USER')
+PASSWORD = os.getenv('PASSWORD')
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': '',
-        'PORT': '',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
+        'ENGINE': ENGINE,
+        'HOST': HOST,
+        'PORT': PORT,
+        'NAME': NAME,
+        'USER': USER,
+        'PASSWORD': PASSWORD,
     }
 }
 
 INSTALLED_APPS = ['datacenter']
-
-SECRET_KEY = 'REPLACE_ME'
 
 TIME_ZONE = 'Europe/Moscow'
 
